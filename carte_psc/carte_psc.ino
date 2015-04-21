b893a373a7235f354d887b4a8dfb680922f0b2cc
Baulieu <b.de.reboul@gmail.com>
2015-04-21 18:05:21 +0200

    création du projet pour la seconde carte, AKA carte_psc_2

--- carte_psc/carte_psc.ino
+++ carte_psc/carte_psc.ino
@@ -1,7 +1,17 @@
-void()
+/*
+il faudra créer des librairies qui se chargeront de tout le taff.
+*/
+
+unsigned long time;
+
+setup()
 {
 }
 
 loop()
 {
+  // on utilise un timestamp pour permettre une synchronisation entre les deux cartes. + attendre un retour, pour etre sur de ne pas rater de tour.
+  time = millis();
+  // à supprimer
+  delay(1000);
 }
