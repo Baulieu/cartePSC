419016cc91c546ff5fa1d0ca38890e1365400342
Baulieu <b.de.reboul@gmail.com>
2015-04-22 00:20:14 +0200

    ajout des bases de la synchronisation et la gestion du burnout par la led 13 pour les deux cartes.

--- carte_psc/carte_psc.ino
+++ carte_psc/carte_psc.ino
@@ -3,15 +3,25 @@ il faudra créer des librairies qui se chargeront de tout le taff.
 */
 
 unsigned long time;
+int duree_loop;
+int led_alerte = 13;
 
 setup()
 {
+  duree_loop = 100; //choisir ici la durée d'une loop !!!doit etre cohérent avec l'autre carte!!!
+  pinMode (led_alert, OUTPUT);
 }
 
 loop()
 {
   // on utilise un timestamp pour permettre une synchronisation entre les deux cartes. + attendre un retour, pour etre sur de ne pas rater de tour.
   time = millis();
-  // à supprimer
-  delay(1000);
+  // sert à gérer la durée d'une loop
+  if (millis() - time < duree_loop) {
+    delay(duree_loop - millis() + time);
+    digitalWrite(led_alert, LOW); //la loop s'est éxecutée en un temps normal
+  }
+  else {
+    digitalWrite(led_alert, HIGH); //la loop met trop de temps à s'éxecuter, la led 13 s'allume
+  }
 }
--- carte_psc_2/carte_psc_2.ino
+++ carte_psc_2/carte_psc_2.ino
@@ -1,7 +1,25 @@
+/**/
+
+unsigned long time;
+int duree_loop;
+int led_alerte = 13;
+
 setup()
 {
+  duree_loop = 100; //choisir ici la durée d'une loop !!!doit etre cohérent avec l'autre carte!!!
+  pinMode(led_alert, OUTPUT);
 }
 
 loop()
 {
+  // on utilise un timestamp pour permettre la synchronisation des deux cartes.
+  time = millis();
+  // sert à gérer la durée d'une loop
+  if (millis() - time < duree_loop) {
+    delay(duree_loop - millis() + time);
+    digitalWrite(led_alert, LOW); //la loop s'est exécutée en un temps normal
+  }
+  else {
+    digitalWrite(led_alert, HIGH); //la loop met trop de temps à s'éxecuter, la led 13 s'allume
+  }
 }
